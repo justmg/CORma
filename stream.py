816cc89560cb701d1d118b10ca50e0325102923a
justmg <aykyassini@gmail.com>
2023-01-31 21:43:13 -0600

    Update stream.py

--- stream.py
+++ stream.py
@@ -1,39 +1,21 @@
 import streamlit as st
-import PyPDF2
-import re
-import webbrowser
+import pandas as pd
+from io import StringIO
 
-def extract_email_from_pdf(pdf_file):
-    # Open the PDF file
-    pdf = PyPDF2.PdfFileReader(pdf_file)
-    
-    # Loop through each page of the PDF
-    for i in range(pdf.getNumPages()):
-        page = pdf.getPage(i)
-        text = page.extractText()
-        
-        # Use a regular expression to find the email address
-        match = re.search(r'[\w\.-]+@[\w\.-]+', text)
-        if match:
-            return match.group(0)
-        
-    return None
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    # To read file as bytes:
+    bytes_data = uploaded_file.getvalue()
+    st.write(bytes_data)
 
-def main():
-    st.set_page_config(page_title="PDF Email Extractor App", page_icon=":guardsman:", layout="wide")
-    st.title("Upload PDF File")
-    file = st.file_uploader("Choose a PDF file", type="pdf")
-    
-    if file is not None:
-        email = extract_email_from_pdf(file)
-        if email:
-            st.success(f"COR email found: {email}")
-            
-            # Add a button to create an email to the COR
-            if st.button("Create Email"):
-                webbrowser.open(f"mailto:{email}")
-        else:
-            st.error("COR email not found")
+    # To convert to a string based IO:
+    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    st.write(stringio)
 
-if __name__ == '__main__':
-    main()
+    # To read file as string:
+    string_data = stringio.read()
+    st.write(string_data)
+
+    # Can be used wherever a "file-like" object is accepted:
+    dataframe = pd.read_csv(uploaded_file)
+    st.write(dataframe)
